4b12cb3ae8f2eddbd5688a31c4dd505a511382c8
LonelyLiaR <lonelyliar8523@gmail.com>
2017-10-17 10:23:15 +0800

    Winner winner, chicken dinner! :trollface:

--- src/components/common/downButton.vue
+++ src/components/common/downButton.vue
@@ -10,35 +10,3 @@
     }
   }
 </script>
-
-<style scoped>
-  .down-button {
-    width: 4rem;
-    height: 4rem;
-    border-radius: 50%;
-    background: transparent;
-    border: 1px solid #fff;
-    position: absolute;
-    bottom: 1.5rem;
-    right: 2rem;
-    mix-blend-mode: difference;
-    z-index: 3
-  }
-  .down-button div {
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    margin-top: .5em
-  }
-  @media only screen and (max-width:414px) {
-    .down-button {
-      height: 2rem;
-      border: 0;
-      left: 50%;
-      right: auto;
-      bottom: 1rem;
-      -webkit-transform: translateX(-50%);
-      transform: translateX(-50%)
-    }
-  }
-</style>
--- src/components/main.vue
+++ src/components/main.vue
@@ -92,6 +92,3 @@
     }
   }
 </script>
-
-<style>
-</style>
